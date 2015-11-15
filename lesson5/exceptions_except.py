fc89664fd75f787b03953d8eac3ec99b6fdf19de
Aleksey Romanov <a_romanov@wargaming.net>
2015-11-16 00:05:52 +0300

    Add y.a. script for showing except working

---
+++ lesson5/exceptions_except.py
@@ -0,0 +1,30 @@
+def take_beer(fridge, number=1):
+    if "beer" not in fridge:
+        raise Exception("No beer at all:(")
+
+    if number > fridge["beer"]:
+        raise Exception("Not enough beer:(")
+
+    fridge["beer"] -= number
+
+
+if __name__ == "__main__":
+    fridge = {
+        "beer": 2,
+        "milk": 1,
+        "meat": 3,
+    }
+    print("I wanna drink 1 bottle of beer...")
+    take_beer(fridge)
+    print("Oooh, great!")
+    print("I wanna drink 2 bottle of beer...")
+    try:
+        take_beer(fridge, 2)
+    except Exception as e:
+        print("Error: {}. Let's continue".format(e))
+
+    print("Fallback. Try to take 1 bottle of beer...")
+    take_beer(fridge, 1)
+    print("Oooh, awesome!")
+
+
