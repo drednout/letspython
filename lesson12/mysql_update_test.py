1d0eea75c9634cf34c0aa711df2147ec345860aa
Aleksey Romanov <a_romanov@wargaming.net>
2016-01-05 16:17:06 +0300

    Add 2 example of update statement

---
+++ lesson12/mysql_update_test.py
@@ -0,0 +1,15 @@
+import MySQLdb
+
+con = MySQLdb.connect('localhost', 'root', '1', 'lesson12');
+
+sql_data = {
+    "id": 1,
+    "code": "USD",
+    "amount": 100,
+}
+sql_query = """UPDATE money SET amount=amount+%(amount)s WHERE player_id=%(id)s AND 
+               code=%(code)s"""
+
+with con:
+    cur = con.cursor()
+    cur.execute(sql_query, sql_data)
